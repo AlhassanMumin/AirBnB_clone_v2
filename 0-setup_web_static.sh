7fc307a84156fea9940020d32aac7fe8737d1b9c
Mumin8 <alhassanmumin8@gmail.com>
2023-01-23 13:26:27 -0800

    web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -24,10 +24,12 @@ server {
     add_header X-Served-By $HOSTNAME;
     root   /var/www/html;
     index  index.html index.htm;
-    location /hbnb_static {
-	alias /data/web_static/current;
-	index index.html index.htm;
+    location /hbnb_static/ {
+    alias /data/web_static/current/;
+    index index.html index.htm;
+    autoindex off;
     }
+
     location /redirect_me {
 	return 301 http://cuberule.com/;
     }
@@ -38,5 +40,5 @@ server {
     }
     
     }
-    events {}
+events {}
 service nginx restart
